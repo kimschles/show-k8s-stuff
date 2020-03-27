0f247e5bf77f2a9f06d63240728768728259ce8e
kimschles <schlesinger.kim@gmail.com>
2020-03-27 16:36:14 -0600

    add notes and print clientset

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-`go mod init github.com/kimschles/show-k8s-stuff`
-`go build -o show-nodes`
-`./show-nodes`
+* `go mod init github.com/kimschles/show-k8s-stuff`
+* `go build -o show-nodes .`
+* `./show-nodes`
--- main.go
+++ main.go
@@ -1,18 +1,71 @@
+// Note: this code is from https://github.com/kubernetes/client-go/blob/master/examples/out-of-cluster-client-configuration/main.go
+
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("hiiii!")
+	var kubeconfig *string
+	// if the home variable is an empty string, set the value to $HOME/.kube/config
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
 	// create the clientset
+	// what is a clientset?
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
-	} else {
-		fmt.Println("In the else statement")
 	}
+	fmt.Println(clientset.CoreV1())
+
+	// for {
+	// 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+	// 	if err != nil {
+	// 		panic(err.Error())
+	// 	}
+	// 	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+	// 	// Examples for error handling:
+	// 	// - Use helper functions like e.g. errors.IsNotFound()
+	// 	// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+	// 	namespace := "default"
+	// 	pod := "example-xxxxx"
+	// 	_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+	// 	if errors.IsNotFound(err) {
+	// 		fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+	// 	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+	// 		fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+	// 			pod, namespace, statusError.ErrStatus.Message)
+	// 	} else if err != nil {
+	// 		panic(err.Error())
+	// 	} else {
+	// 		fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+	// 	}
+
+}
+
+func homeDir() string {
+	//find and save the value of $HOME
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
 }
